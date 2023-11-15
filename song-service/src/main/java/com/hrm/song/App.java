33be6459d355a26c4d67b5cf37f6daafde8898af
herman2lv <herman2lv@gmail.com>
2023-11-15 16:06:27 +0300

    Add eureka discovery clients to services

--- song-service/src/main/java/com/hrm/song/App.java
+++ song-service/src/main/java/com/hrm/song/App.java
@@ -2,8 +2,10 @@ package com.hrm.song;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class App {
 
     public static void main(String[] args) {
